6deb7b533c9d845fe930a6af2d4ef1cc9b080edb
glow <renancavenaghizuri@gmail.com>
2024-06-09 20:20:26 -0300

    disabling turbo stream

--- app/javascript/devise.js
+++ app/javascript/devise.js
@@ -1,8 +1,5 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
 import * as bootstrap from "bootstrap"
+Turbo.session.drive = false
 
-//= require rails-ujs
-//= require turbolinks
-//= require_tree .
