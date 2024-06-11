7bd801d893678b4be268a5bf79e45b944e9f1b83
glow <renancavenaghizuri@gmail.com>
2024-06-10 23:01:04 -0300

    adding phone and name to user devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,15 @@ class ApplicationController < ActionController::Base
       "application"
     end
   end
+
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+       def configure_permitted_parameters
+            devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password,:password_confirmation, :phone)}
+        
+        end
+
 end
